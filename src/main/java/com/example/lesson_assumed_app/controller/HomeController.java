597b618fdcaf7f94cd42b7d009927c04458e9375
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2024-12-19 00:29:31 +0900

    Add:登録画面、参照画面、編集画面のモック作成 -トップ画面から登録画面と参照画面へリンクを作成 -参照画面からトップ画面と編集画面へのリンクを作成 -編集画面からトップ画面へのリンクを作成 -登録画面からトップ画面へのリンクを作成 -HomeController.javaにて登録画面、参照画面、編集画面の取得処理を追加

--- src/main/java/com/example/lesson_assumed_app/controller/HomeController.java
+++ src/main/java/com/example/lesson_assumed_app/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.lesson_assumed_app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
     @GetMapping("/")
@@ -11,5 +12,17 @@ public class HomeController {
     @GetMapping("/home")
     public String home(){
         return "home";
+    }
+    @GetMapping("/read")
+    public String read(){
+        return "read";
+    }
+    @GetMapping("/update")
+    public String update(){
+        return "update";
+    }
+    @GetMapping("/create")
+    public String create() {
+        return "create";
     }
 }
