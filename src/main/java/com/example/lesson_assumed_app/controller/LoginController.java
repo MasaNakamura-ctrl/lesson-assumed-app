809d2db83299ef6f59bdd05b33dfc5e42522c775
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2024-12-29 00:04:29 +0900

    Update:LoginController.javaの調整 -リクエストパラムを追加 -記録一覧画面への遷移を追加

--- src/main/java/com/example/lesson_assumed_app/controller/LoginController.java
+++ src/main/java/com/example/lesson_assumed_app/controller/LoginController.java
@@ -1,11 +1,21 @@
 package com.example.lesson_assumed_app.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     @GetMapping("/")
     public String login(){
         return "login";
+    }
+    @PostMapping("/check")
+    public String postIdPassword(
+        @RequestParam("id") String id,
+        @RequestParam("password") String password,
+        Model model){
+        return "home";
     }
 }
