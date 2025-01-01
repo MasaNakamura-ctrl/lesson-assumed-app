a36c186b34d40897f608a3b8dd17a01688453b41
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-01-01 10:52:53 +0900

    Update:ReadController.javaの調整 -例外処理でDBからのレコード取得処理を追加

--- src/main/java/com/example/lesson_assumed_app/controller/ReadController.java
+++ src/main/java/com/example/lesson_assumed_app/controller/ReadController.java
@@ -1,5 +1,8 @@
 package com.example.lesson_assumed_app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +10,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.lesson_assumed_app.entity.ReadEntity;
+import com.example.lesson_assumed_app.model.Lesson;
 import com.example.lesson_assumed_app.repository.ReadRepository;
+import com.example.lesson_assumed_app.service.HomeService;
 
 @Controller
 @RequestMapping("/read")
 public class ReadController {
     private final ReadRepository readRepository;
+
+    @Autowired
+    private HomeService homeService;
 
     public ReadController(ReadRepository readRepository){
         this.readRepository = readRepository;
@@ -27,6 +35,9 @@ public class ReadController {
             return "read";
         } catch(IllegalArgumentException e){
             // 例外処理として記録一覧画面へ再度遷移
+            // DBからレッスン記録を一通り取得
+            List<Lesson> lessons = homeService.getAllLessons();
+            model.addAttribute("lessons", lessons);
             return "home";
         }
     }
